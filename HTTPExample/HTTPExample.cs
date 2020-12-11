42aa1d6286e1100ddfa74224217499638396cecd
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2020-12-11 13:48:16 +0100

    Přidejte soubory projektu.

---
+++ HTTPExample/HTTPExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json; //NuGet, který obohacuje projekt o json -> Spravovat NuGety -> vyhledat newtonsoft (stačí napsat json)
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPExample
+{
+    public class Person
+    {
+        [JsonProperty("Jméno")] //Přepisuje název vlastnosti třídy
+        public string Name { get; set; }
+
+        [JsonProperty("Příjmení")]
+        public string LastName { get; set; }
+
+        [JsonProperty("Věk")]
+        public int Age { get; set; }
+        public override string ToString() => $"Jsem {Name} {LastName} a je mi {Age} let.";
+    }
+    class Program
+    {
+        static async Task Main(string[] args) //kvůli posílání informací na web -> nezapomenout using
+        {
+            Person Dalsi = new Person { Name = "Martin", LastName = "Stránský", Age = 24 };
+            var JsonDalsi = JsonConvert.SerializeObject(Dalsi); //Převede proměnnou na formát json
+            Person DeJsonDalsi = JsonConvert.DeserializeObject<Person>(JsonDalsi); //Převede formát json na libovolný, který chceme (ovšem musí fungovat konverze)
+
+            HttpClient htc = new HttpClient();
+            HttpContent htcContent = new StringContent(JsonDalsi, Encoding.UTF8, "application/json"); //identifikátor, který říká, jakého typu je informace, kterou posílám (konkrétně json)
+            HttpResponseMessage hrm = await htc.PostAsync("http://ptsv2.com/t/ssps", htcContent);
+
+            Console.WriteLine(hrm);
+        }
+    }
+}
---
+++ _JSON/_JSON.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace _JSON
+{
+    public class Person
+    {
+        [JsonProperty("Jméno")]
+        public string Name { get; set; }
+
+        [JsonProperty("Příjmení")]
+        public string LastName { get; set; }
+
+        [JsonProperty("Věk")]
+        public int Age { get; set; }
+        public override string ToString() => $"Jsem {Name} {LastName} a je mi {Age} let.";
+    }
+    public class Pair
+    {
+        [JsonProperty("Otec")]
+        public Person Osoba1 { get; set; }
+
+        [JsonProperty("Matka")]
+        public Person Osoba2 { get; set; }
+        public override string ToString() => $"Jméno : {Osoba1.Name}\nPříjmení : {Osoba1.LastName}\nVěk : {Osoba1.Age}\n\nJméno : {Osoba2.Name}\nPříjmení : {Osoba2.LastName}\nVěk : {Osoba2.Age}";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string Output = JsonConvert.SerializeObject(Osoba);
+            Person Alex = new Person { Name = "Alexandr", LastName = "Sekera", Age = 18 };
+            Person Jirka = new Person { Name = "Jiří", LastName = "Měřínský", Age = 45 };
+            Pair Par = new Pair { Osoba1 = Alex, Osoba2 = Jirka };
+            var JsonPerson1 = JsonConvert.SerializeObject(Alex);
+            var JsonPerson2 = JsonConvert.SerializeObject(Jirka);
+            var JsonPar = JsonConvert.SerializeObject(Par);
+            Console.WriteLine(JsonPerson1);
+            Console.WriteLine(JsonPerson2);
+
+            Pair c = JsonConvert.DeserializeObject<Pair>(JsonPar);
+            Console.WriteLine(c);
+        }
+    }
+}
